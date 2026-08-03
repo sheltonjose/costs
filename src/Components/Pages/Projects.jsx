8ade77b969fc93e0c108b62a407dc95f3a7dc2c6
Shelton <198606617+sheltonjose@users.noreply.github.com>
2026-08-03 21:06:25 +0200

    Update Projects.jsx

--- src/Components/Pages/Projects.jsx
+++ src/Components/Pages/Projects.jsx
@@ -1,16 +1,30 @@
 import {useLocation} from 'react-router-dom'
 import Message from "../Layout/Message"
+import Container from '../Layout/Container'
+import LinkButton from '../Layout/LinkButton'
+
+import styles from './Projects.module.css'
 
 function Projects(){
 
     const location =  useLocation()
+    let message= ''
 
-
+    if(location.state){
+        message = location.state.message
+    }
     //pagina que exige todos projectos cadastrados
     return (
-        <div>
-            <h1>Meus Pojectos</h1>
-            <Message msg="Alguma mensagem" />
+        <div className={styles.project_container}>
+           <div className={styles.title_container}>
+                <h1>Meus Projectos</h1>
+                 <LinkButton to="/newproject" text="criar projecto"/>
+           </div>
+           {message && <Message type="sucess" msg={message} />}
+           <Container customClass="start">
+                <p>Projectos...</p>
+
+           </Container>
         </div>
     )
 }
