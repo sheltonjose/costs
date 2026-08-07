5fd8938af3d0fac6b90b48ffda1ae36511006c9b
Shelton <198606617+sheltonjose@users.noreply.github.com>
2026-08-07 20:57:40 +0200

    Update Select.js

--- src/Components/Form/Select.js
+++ src/Components/Form/Select.js
@@ -4,8 +4,8 @@ function Select({text,name, options, handleOnChange, value}){
     return(
         <div className={styles.form_control}>
             <label htmlFor={name} className={styles.form_control_label}>{text}:</label>
-            <select name={name} id={name} onChange={handleOnChange} value={value || ''}>
-                <option>Selecione uma opção</option>
+            <select name={name} id={name} onChange={handleOnChange} value={value || ''} required>
+                <option value={""} disabled>Selecione uma opção</option>
                 {options.map((option)=>(
                     <option key={option.id} value={option.id} >{option.name}</option>
                 ))}
