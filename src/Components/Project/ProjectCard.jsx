dd1a53dfdfce6cc8e4b2b9c3e6dc4e1e3340e725
Shelton <198606617+sheltonjose@users.noreply.github.com>
2026-08-07 20:57:10 +0200

    Update ProjectCard.jsx

--- src/Components/Project/ProjectCard.jsx
+++ src/Components/Project/ProjectCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import styles from './ProjectCard.module.css'
 
 import { BsPencil, BsFillTrashFill } from 'react-icons/bs'
@@ -11,11 +12,15 @@ function ProjectCard({id,name,budget,category,handleRemove}){
                 <span>Orçamento</span> {budget} MZN
             </p>
             <p className={styles.category_text}>
-                <span></span> {category}
+                <span className={`${styles[category.toLowerCase()]}`}></span> {category}
             </p>
-            <div>
-                <p>Editar </p>
-                <p>Remover</p>
+            <div className={styles.project_card_actions}>
+                <Link to="/">
+                    <BsPencil/>Editar
+                </Link>
+                <button >
+                    <BsFillTrashFill/>Excluir
+                </button>
             </div>
         </div>
     )
