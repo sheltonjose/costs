02a54cb6336d5c5ff52358937b616e49b6945259
Shelton <198606617+sheltonjose@users.noreply.github.com>
2026-08-09 16:21:30 +0200

    Update Home.js

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,5 +1,5 @@
 import styles from './Home.module.css'
-import saving from '../../img/saving.png'
+import LilPig from  '../../img/LilPig.png'
 import LinkButton from '../Layout/LinkButton'
 
 
@@ -9,7 +9,7 @@ function Home(){
             <h1>Bem-vindo ao <span>Costs</span></h1>
             <p>Comece a gerenciar os seus projectos agora mesmo!</p>
            <LinkButton to="/newproject" text="criar projecto"/>
-            <img src={saving} alt='Costs'></img>
+            <img src={LilPig} alt='Costs'></img>
 
         </section>
     )
